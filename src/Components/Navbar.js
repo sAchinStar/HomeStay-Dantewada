694333c43e15d25dc78cad5352f456ad42085462
Somda123 <somdakawasi23@navgurukul.org>
2024-12-20 17:07:39 +0530

    fix:navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Navbar</a>
+                <Link class="navbar-brand" to="/">Navbar</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link class="nav-link active" aria-current="page" to="/home">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">contact us</a>
+                            <Link class="nav-link" to="/book">Book</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/listings">Listings</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/contact">Contact</Link>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
