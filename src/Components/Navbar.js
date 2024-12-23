94184c31ebd197f8b6b189a32e7cd53aef56fb5f
sAchinStar <sachin680125@gmail.com>
2024-12-23 18:08:46 +0530

    changed logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css"
-import logo from"../Assets/logo.png"
+import logo from"../Assets/logo.jpg"
 
 function Navbar() {
     return (
