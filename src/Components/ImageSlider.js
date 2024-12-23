d7fbd09554df2f92d522cb7ddfc31621c0e25560
sAchinStar <sachin680125@gmail.com>
2024-12-23 17:57:21 +0530

    changed some images

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,25 +1,23 @@
 import img1 from "../Assets/image1.png"
 import img2 from "../Assets/image2.png"
 import img3 from "../Assets/image3.png"
-import photo1 from "../Assets/1.jpg"
-import photo2 from "../Assets/2.jpg"
-import photo3 from "../Assets/3.jpg"
+import photo1 from "../Assets/1.png"
+import photo2 from "../Assets/2.png"
+import photo3 from "../Assets/3.png"
 import photo4 from "../Assets/4.jpg"
 import photo5 from "../Assets/5.jpg"
 import photo6 from "../Assets/6.jpg"
-import photo7 from "../Assets/7.jpg"
+import photo7 from "../Assets/7.png"
 import photo8 from "../Assets/8.jpg"
 import photo9 from "../Assets/9.jpg"
 import photo10 from "../Assets/10.jpg"
 import photo11 from "../Assets/11.jpg"
 import photo12 from "../Assets/12.jpg"
 import photo13 from "../Assets/13.jpg"
-// import photo14 from "../Assets/14.jpg"
+import photo14 from "../Assets/14.jpg"
 import photo15 from "../Assets/15.jpg"
-// import photo16 from "../Assets/16.jpg"
+import photo16 from "../Assets/16.jpg"
 import photo17 from "../Assets/17.jpg"
-import photo18 from "../Assets/18.jpg"
-import photo19 from "../Assets/19.jpg"
 function ImageSlider() {
 
     return (
@@ -76,22 +74,16 @@ function ImageSlider() {
                         <img src={photo13} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
                     </div>
                     <div className="carousel-item">
-                        {/* <img src={photo14} className="d-block w-100" alt="..." style={{height:28 +"rem"}} /> */}
+                        <img src={photo14} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
                     </div>
                     <div className="carousel-item">
                         <img src={photo15} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
                     </div>
                     <div className="carousel-item">
-                        {/* <img src={photo16} className="d-block w-100" alt="..." style={{height:28 +"rem"}} /> */}
+                        <img src={photo16} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
                     </div>
                     <div className="carousel-item">
                         <img src={photo17} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
-                    </div>
-                    <div className="carousel-item">
-                        <img src={photo18} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
-                    </div>
-                    <div className="carousel-item">
-                        <img src={photo19} className="d-block w-100" alt="..." style={{height:28 +"rem"}} />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
