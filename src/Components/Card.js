77ae809ed54e2c4c2237e832a0ea4ac519f60711
sAchinStar <sachin680125@gmail.com>
2024-12-24 10:37:33 +0530

    Update DetailsModal

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,24 +2,24 @@
 import DetailsPage from "./DetailsPage";
 
 
-function Card({picture,title,description}) {
+function Card({ picture, title, description }) {
+    const uniqueModalId = `modal-${title.replace(/\s+/g, '-').toLowerCase()}`; // Create a unique ID
+  
     return (
-        <>
-            <div className="card my-3" style={{ width: 18 + "rem" ,height:24+ "rem" }}>
-                <img src={picture} className="card-img-top" alt="No Cover" style={{ width: 18 + "rem" ,height:10+ "rem" }} />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description.length>=100?description.slice(0,100) + "...":description}</p>
-                    {/* <Link to="#" className="btn btn-primary">View Details</Link> */}
-                    {/* // < !--Button trigger modal-- > */}
-                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        View Details
-                    </button>
-                </div>
-            </div>
-            <DetailsPage />
-        </>
-    )
-}
-
+      <>
+        <div className="card my-3" style={{ width: "18rem", height: "24rem" }}>
+          <img src={picture} className="card-img-top" alt="No Cover" style={{ width: "18rem", height: "10rem" }} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description.length >= 100 ? description.slice(0, 100) + "..." : description}</p>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#${uniqueModalId}`}>
+              View Details
+            </button>
+          </div>
+        </div>
+        <DetailsPage id={uniqueModalId} title={title} description={description} picture={picture} />
+      </>
+    );
+  }
+  
 export default Card;
--- src/Components/DetailsPage.js
+++ src/Components/DetailsPage.js
@@ -1,38 +1,19 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { fetchHomestays } from "../api";
-
-const DetailsPage = () => {
-  // const { id } = useParams(); // Get the homestay ID from the route
-  // const [homestay, setHomestay] = useState(null);
-
-  // useEffect(() => {
-  //   fetchHomestays().then((data) => {
-  //     const selectedHomestay = data.find((item) => item.id === id);
-  //     setHomestay(selectedHomestay);
-  //   });
-  // }, [id]);
-
-  // if (!homestay) {
-  //   return <p>Loading...</p>;
-  // }
-
+const DetailsPage = ({ id, title, description, picture }) => {
   return (
     <>
-
-{/* // <!--Modal --> */}
-      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div className="modal fade" id={id} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby={`${id}-label`} aria-hidden="true">
         <div className="modal-dialog modal-xl">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+              <h1 className="modal-title fs-5" id={`${id}-label`}>{title}</h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div className="modal-body">
-              ...
+            <div className="modal-body text-center">
+              <img style={{width:"38rem", height:"18rem"}} src={picture} alt="No cover" />
+              <p>Description:{description}</p>
+            <p>Price Rs 1000/-</p>
             </div>
             <div className="modal-footer d-flex justify-content-center">
-              {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
               <button type="button" className="btn btn-success">Book Now</button>
             </div>
           </div>
@@ -41,5 +22,6 @@ const DetailsPage = () => {
     </>
   );
 };
+
 
 export default DetailsPage;
