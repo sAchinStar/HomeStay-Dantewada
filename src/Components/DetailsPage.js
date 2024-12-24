1141c4e63abe67ac20c2b9806e65d0ff027fc7a7
sAchinStar <sachin680125@gmail.com>
2024-12-24 15:17:26 +0530

    fix: book button of detail page

--- src/Components/DetailsPage.js
+++ src/Components/DetailsPage.js
@@ -1,7 +1,35 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const DetailsPage = ({ id, title, description, picture }) => {
+  const navigation = useNavigate();
+
+  const handleLink = () => {
+    // Hide the modal using Bootstrap's modal API
+    const modalElement = document.getElementById(id);
+    const modalInstance = window.bootstrap.Modal.getInstance(modalElement);
+    if (modalInstance) {
+      modalInstance.hide(); // Dismiss the modal
+    }
+  
+    // Scroll to the top
+    window.scrollTo(0, 0);
+  
+    // Navigate to the book page
+    navigation('/book');
+  };
+  
+
   return (
     <>
-      <div className="modal fade" id={id} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby={`${id}-label`} aria-hidden="true">
+      <div
+        className="modal fade"
+        id={id}
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby={`${id}-label`}
+        aria-hidden="true"
+      >
         <div className="modal-dialog modal-xl">
           <div className="modal-content">
             <div className="modal-header">
@@ -9,12 +37,18 @@ const DetailsPage = ({ id, title, description, picture }) => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body text-center">
-              <img style={{width:"38rem", height:"18rem"}} src={picture} alt="No cover" />
-              <p>Description:{description}</p>
-            <p>Price Rs 1000/-</p>
+              <img
+                style={{ width: "38rem", height: "18rem" }}
+                src={picture}
+                alt="No cover"
+              />
+              <p>Description: {description}</p>
+              <p>Price Rs 1000/-</p>
             </div>
             <div className="modal-footer d-flex justify-content-center">
-              <button type="button" className="btn btn-success">Book Now</button>
+              <button onClick={handleLink} type="button" className="btn btn-success">
+                Book Now
+              </button>
             </div>
           </div>
         </div>
@@ -22,6 +56,5 @@ const DetailsPage = ({ id, title, description, picture }) => {
     </>
   );
 };
-
 
 export default DetailsPage;
