57180a87b708bfaf2a18f8402d8dcec52673f0ec
Somda123 <somdakawasi23@navgurukul.org>
2024-12-25 13:09:14 +0530

    feat/protect

---
+++ src/Components/ProtectRouter.jsx
@@ -0,0 +1,8 @@
+import { Outlet,Navigate } from "react-router-dom";
+
+const ProtectRouter =() =>{
+const user =null
+    return user ? <Outlet/> : <Navigate to="/login-page" />
+}
+
+export default ProtectRouter;
